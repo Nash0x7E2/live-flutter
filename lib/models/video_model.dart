a5c5a4d201faa8afdb2c47f9dd5641728310ab5b
Neevash Ramdial <mail@neevash.dev>
2020-12-06 21:22:09 -0400

    add thumbnailUrl to Video

--- lib/models/video_model.dart
+++ lib/models/video_model.dart
@@ -7,6 +7,7 @@ class Video {
     @required this.duration,
     @required this.createdAt,
     @required this.playbackUrl,
+    @required this.thumbnailUrl,
   });
 
   factory Video.fromMap(Map<String, dynamic> map) {
@@ -17,16 +18,21 @@ class Video {
       duration: Duration(seconds: (map['duration'] as double).toInt()),
       createdAt: DateTime.parse(map['created_at']),
       playbackUrl: "https://stream.mux.com/$playback.m3u8",
+      thumbnailUrl:
+          "https://image.mux.com/$playback/thumbnail.png?width=1920&height=1080&fit_mode=pad",
     );
   }
 
   @override
   String toString() {
     return 'Video{playbackUrl: $playbackUrl, playbackId: $playbackId, '
-        'assetId: $assetId, duration: $duration, createdAt: $createdAt}';
+        'assetId: $assetId, duration: $duration, createdAt: $createdAt '
+        'thumbnailUrl: $thumbnailUrl'
+        '}';
   }
 
   final String playbackUrl;
+  final String thumbnailUrl;
   final String playbackId;
   final String assetId;
   final Duration duration;
